9f12ca83632ac072a0caf1e2da3a284e8834448f
tmichaell <tomlesperance93@gmail.com>
2024-05-18 13:28:54 -0500

    Work on notes function

---
+++ front-end/src/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header({ headerText }) {
+  return (
+    <>
+      <div className="row m-1 border">
+        <h2>{headerText}</h1>
+      </div>
+    </>
+  );
+}
---
+++ front-end/src/layout/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="row m-1">
+      <h1>Character Sheet Tool</h1>
+      <h3>for Old-School RPGs!</h3>
+    </div>
+  );
+}
